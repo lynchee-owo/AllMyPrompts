9a1add2d68acc62f55663dd42200565e721ccd37
lynchee-owo <lynchee.owo@gmail.com>
2023-05-12 17:39:13 -0400

    created header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import { Box, Typography, List, ListItem, ListItemText, IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-
+import Header from './components/Header';
+import Clear from '@mui/icons-material/Clear';
 
 function App() {
   const [prompts, setPrompts] = useState([]);
+  const [deleteMode, setDeleteMode] = useState(false);
 
   useEffect(() => {
     chrome.storage.sync.get(null, function(items) {
@@ -40,30 +42,34 @@ function App() {
   
 
   const copyToClipboard = (text, key) => {
-    navigator.clipboard.writeText(text)
-      .then(() => {
-        console.log(`Copied: ${text}`);
-        // Increase the count and update the storage
-        chrome.storage.sync.get(key, function(result) {
-          let newPrompt = result[key];
-          newPrompt.count++;
-          newPrompt.copied = true; // Set copied to true
-          chrome.storage.sync.set({ [key]: newPrompt }, function() {
-            // Update the local state
-            setPrompts(prevPrompts => prevPrompts.map(prompt => prompt.key === key ? { key, ...newPrompt } : prompt));
-            // Set copied back to false after 1 second
-            setTimeout(() => {
-              newPrompt.copied = false;
-              chrome.storage.sync.set({ [key]: newPrompt }, function() {
-                setPrompts(prevPrompts => prevPrompts.map(prompt => prompt.key === key ? { key, ...newPrompt } : prompt));
-              });
-            }, 500);
+    if (deleteMode) {
+      deletePrompt(key);
+    } else {
+      navigator.clipboard.writeText(text)
+        .then(() => {
+          console.log(`Copied: ${text}`);
+          // Increase the count and update the storage
+          chrome.storage.sync.get(key, function(result) {
+            let newPrompt = result[key];
+            newPrompt.count++;
+            newPrompt.copied = true; // Set copied to true
+            chrome.storage.sync.set({ [key]: newPrompt }, function() {
+              // Update the local state
+              setPrompts(prevPrompts => prevPrompts.map(prompt => prompt.key === key ? { key, ...newPrompt } : prompt));
+              // Set copied back to false after 1 second
+              setTimeout(() => {
+                newPrompt.copied = false;
+                chrome.storage.sync.set({ [key]: newPrompt }, function() {
+                  setPrompts(prevPrompts => prevPrompts.map(prompt => prompt.key === key ? { key, ...newPrompt } : prompt));
+                });
+              }, 500);
+            });
           });
+        })
+        .catch(err => {
+          console.error('Could not copy text: ', err);
         });
-      })
-      .catch(err => {
-        console.error('Could not copy text: ', err);
-      });
+    }
   }
 
   const deletePrompt = (key) => {
@@ -79,15 +85,8 @@ function App() {
 
   return (
     <div className="App">
+      <Header setDeleteMode={setDeleteMode} /> 
       <Box p={2}>
-        <Typography variant="h6" component="div">My Prompts</Typography>
-        <IconButton
-          edge="end"
-          aria-label="delete"
-          onClick={(event) => {}}
-        >
-          <DeleteIcon />
-        </IconButton>
         <List>
           {prompts.map(({ text, key, copied, editing }) => (
             <ListItem key={key} disableGutters>
@@ -115,14 +114,18 @@ function App() {
                 )}
                 <IconButton
                   edge="end"
-                  aria-label="edit"
+                  aria-label={deleteMode ? "delete" : "edit"}
                   className="edit-button"
                   onClick={(event) => {
                     event.stopPropagation();
-                    toggleEdit(key);
+                    if (deleteMode) {
+                      deletePrompt(key);
+                    } else {
+                      toggleEdit(key);
+                    }
                   }}
                 >
-                  <EditIcon />
+                  {deleteMode ? <Clear /> : <EditIcon />}
                 </IconButton>
               </Box>
             </ListItem>
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, Typography, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const Header = ({ setDeleteMode }) => {
+  return (
+    <Box p={2} display="flex" justifyContent="space-between" alignItems="center" className="header">
+      <Typography variant="h6" component="div">My Prompts</Typography>
+      <IconButton
+        edge="end"
+        className="delete-button"
+        aria-label="delete"
+        onClick={() => setDeleteMode(prev => !prev)}
+      >
+        <DeleteIcon />
+      </IconButton>
+    </Box>
+  );
+}
+
+export default Header;
